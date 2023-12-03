39ccc955e3dbc755935d4b72446fbca8400b1d31
Max Borghino <fmborghino@gmail.com>
2023-12-02 18:34:48 -0800

    Day02

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,4 +1,10 @@
-fun part1(lines: List<String>):Int {
+data class Pick(val red: Int, val green: Int, val blue: Int) // this could just be a map
+data class Game(val id: Int, val picks: List<Pick>)
+
+fun log(message: Any?) {
+    println(message)
+}
+
 /*
     (1) Parse file of Games that looks like:
         Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
@@ -23,9 +29,73 @@ fun part1(lines: List<String>):Int {
 
     (3) sum the game IDs.
 */
-
-   return 0
+fun part1(lines: List<String>, given: Pick):Int {
+    val games: List<Game> = lines.map { Game(it) }
+    val filteredGames = games.filter { game ->
+        game.picks.all { pick -> pick.red <= given.red } &&
+                game.picks.all { pick -> pick.green <= given.green } &&
+                game.picks.all { pick -> pick.blue <= given.blue }
+    }
+    return filteredGames.sumOf { it.id }
 }
+
+fun Game(line: String): Game {
+    // parse a line like this to get a Game
+    // Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+    val parts = line.split(":")
+    val gameId = parts[0].trim().substring(5).toInt()
+    val picks = parts[1].split(";").map { it.trim() }.map { Pick(it) }
+    return Game(gameId, picks)
+}
+
+fun Pick(pick: String): Pick {
+    // parse line like this to get a Pick
+    // 1 red, 2 green, 6 blue
+    // missing color means 0
+    val colors: List<String> = pick.split(",").map { it.trim() }
+    var red = 0 // yuk, vars
+    var green = 0
+    var blue = 0
+    colors.forEach {
+        val (count, color) = it.split(" ")
+        when (color) {
+            "red" -> red = count.toInt()
+            "green" -> green = count.toInt()
+            "blue" -> blue = count.toInt()
+        }
+    }
+
+    return Pick(red, green, blue)
+}
+
+/*
+ * for each Game(val id: Int, val picks: List<Pick>)
+ * find the maximum red, green, blue from the list of Pick
+ * then do the math for the result
+ */
+fun part2(lines: List<String>): Int {
+    val games: List<Game> = lines.map { Game(it) }
+    val maxPicks = games.map { game ->
+        val maxRed = game.picks.maxByOrNull { it.red }
+        val maxGreen = game.picks.maxByOrNull { it.green }
+        val maxBlue = game.picks.maxByOrNull { it.blue }
+        Pick(maxRed?.red ?: 1, maxGreen?.green ?: 1, maxBlue?.blue ?: 1)
+    }
+    val powers = maxPicks.map { it.red * it.green * it.blue }
+    return powers.sum()
+}
+
 fun main() {
-    verify("Test part 1", part1(readInput("test/Day02.txt")), 8)
+    val given = Pick(12, 13, 14)
+    verify("Test part 1", part1(readInput("test/Day02.txt"), given), 8)
+
+//    verify("Winston part 1", part1(readInput("ww/Day02.txt"), given), 999)
+
+    verify("Max part 1", part1(readInput("mb/Day02.txt"), given), 1853)
+
+    verify("Test part 2", part2(readInput("test/Day02.txt")) , 2286)
+
+//    verify("Winston part 2", part2(readInput("ww/Day02.txt")), 999)
+
+    verify("Max part 2", part2(readInput("mb/Day02.txt")), 72706)
 }
--- src/Utils.kt
+++ src/Utils.kt
@@ -24,9 +24,18 @@ fun Any?.log() = println(this)
 
 val COLOR_GREEN = "\u001B[0;32m"
 val COLOR_RED = "\u001B[0;31m"
+val COLOR_YELLOW = "\u001B[0;33m"
 val COLOR_RESET = "\u001B[0m"
 fun verify(msg: String, actual: Int, expected: Int):Unit {
     val output = if (actual == expected) "$msg\n    ${COLOR_GREEN}$actual == $expected — OK ${COLOR_RESET}"
     else "$msg\n    ${COLOR_RED}$actual != $expected — FAIL ${COLOR_RESET}"
     println(output)
+}
+
+// handy if you want to print the result of an expression AND pass that result back into your call site
+// use like: tee { expression }
+fun <T> tee(expression: () -> T): T {
+    val result = expression()
+    println("${COLOR_YELLOW}$result${COLOR_RESET}")
+    return result
 }
